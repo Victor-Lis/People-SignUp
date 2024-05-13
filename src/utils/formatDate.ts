65c0dbfd03faa0686d59f99cc68b38197169710e
Victor-Lis <victorlisbronzo1@gmail.com>
2024-05-13 10:49:47 -0300

    v1.13
    - Adicionando AsyncStorage e já utilizando para armazenar dados locais

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,5 +2,5 @@ import { formatNum } from "./formatNum";
 
 export const formatDate = (date: Date) =>
     `${formatNum(date.getDate())}/${formatNum(
-      date.getMonth() + 1
+      date.getMonth()
     )}/${date.getFullYear()}`;
