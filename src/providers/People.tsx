6a461769101130f3ddaa7a55ddca3a389604fa90
Victor-Lis <victorlisbronzo1@gmail.com>
2024-05-13 11:40:39 -0300

    v1.16
    - Melhorando identação do código;
    - Ajustando função pushPerson para que tanto o armazenamento local, tanto o em "variáveis" tenha o mesmo id

--- src/providers/People.tsx
+++ src/providers/People.tsx
@@ -1,63 +1,75 @@
-import { ReactNode, createContext, useEffect, useState } from 'react';
-import { PersonType } from '../@types/person';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { showToast } from '../utils/showToast';
+import { ReactNode, createContext, useEffect, useState } from "react";
+import { PersonType } from "../@types/person";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { showToast } from "../utils/showToast";
 
 type PersonContext = {
-  people: PersonType[]
-  pushPerson: ({name, CPF, birthday}: Omit<PersonType, "id">) => void
-  updatePerson: ({id, name, CPF, birthday}: PersonType) => void
-  removePerson: ({id}:{id: string}) => void
-  loadAsyncData: () => void
-}
+  people: PersonType[];
+  pushPerson: ({ name, CPF, birthday }: Omit<PersonType, "id">) => void;
+  updatePerson: ({ id, name, CPF, birthday }: PersonType) => void;
+  removePerson: ({ id }: { id: string }) => void;
+  loadAsyncData: () => void;
+};
 
-export const PeopleContext = createContext({} as PersonContext)
+export const PeopleContext = createContext({} as PersonContext);
 
-export default function PeopleProvider({children}:{children: ReactNode}) {
- const [people, setPeople] = useState<PersonType[]>([])
+export default function PeopleProvider({ children }: { children: ReactNode }) {
+  const [people, setPeople] = useState<PersonType[]>([]);
 
- const pushPerson = ({name, CPF, birthday}: Omit<PersonType, "id">) => {
-  setPeople((oldPeople) => [...oldPeople, {name, CPF, birthday, id: `${Math.random()}`}])
-  saveAsyncData([...people, {name, CPF, birthday, id: `${Math.random()}`}])
- }
+  const pushPerson = ({ name, CPF, birthday }: Omit<PersonType, "id">) => {
+    let id = `${Math.random()}`;
+    setPeople((oldPeople) => [...oldPeople, { name, CPF, birthday, id }]);
+    saveAsyncData([...people, { name, CPF, birthday, id }]);
+  };
 
- const updatePerson = ({id, name, CPF, birthday}: PersonType) => {
-  setPeople((oldPeople) => [...oldPeople.filter(person => person.id !== id), {name, CPF, birthday, id}])
-  saveAsyncData([...people.filter(person => person.id !== id), {name, CPF, birthday, id}])
- }
- 
- const removePerson = ({id}:{id: string}) => {
-  setPeople((oldPeople) => [...oldPeople.filter(person => person.id !== id)])
-  saveAsyncData([...people.filter(person => person.id !== id)])
- }
+  const updatePerson = ({ id, name, CPF, birthday }: PersonType) => {
+    setPeople((oldPeople) => [
+      ...oldPeople.filter((person) => person.id !== id),
+      { name, CPF, birthday, id },
+    ]);
+    saveAsyncData([
+      ...people.filter((person) => person.id !== id),
+      { name, CPF, birthday, id },
+    ]);
+  };
 
- const loadAsyncData = async () => {
-  try {
-    const storedData = await AsyncStorage.getItem('@people');
-    if (storedData !== null) {
-      const parsedData: PersonType[] = JSON.parse(storedData);
-      parsedData.forEach((person) => {
-        person.birthday = new Date(person.birthday); 
-      });
-      setPeople(parsedData);
+  const removePerson = ({ id }: { id: string }) => {
+    setPeople((oldPeople) => [
+      ...oldPeople.filter((person) => person.id !== id),
+    ]);
+    saveAsyncData([...people.filter((person) => person.id !== id)]);
+  };
+
+  const loadAsyncData = async () => {
+    try {
+      const storedData = await AsyncStorage.getItem("@people");
+      if (storedData !== null) {
+        const parsedData: PersonType[] = JSON.parse(storedData);
+        parsedData.forEach((person) => {
+          person.birthday = new Date(person.birthday);
+        });
+        setPeople(parsedData);
+      }
+      showToast("Dados locais foram pegos!");
+    } catch (error) {
+      console.error("Error loading data from AsyncStorage:", error);
     }
-    showToast("Dados locais foram pegos!")
-  } catch (error) {
-    console.error('Error loading data from AsyncStorage:', error);
-  }
-};
+  };
 
-const saveAsyncData = async (p: PersonType[]) => {
-  try {
-    const stringifiedData = await JSON.stringify(p);
-    await AsyncStorage.setItem('@people', stringifiedData);
-  } catch (error) {
-    console.error('Error saving data to AsyncStorage:', error);
-  }
-};
- return (
-   <PeopleContext.Provider value={{people, pushPerson, updatePerson, removePerson, loadAsyncData}}>
-    {children}
-   </PeopleContext.Provider>
+  const saveAsyncData = async (p: PersonType[]) => {
+    try {
+      const stringifiedData = await JSON.stringify(p);
+      await AsyncStorage.setItem("@people", stringifiedData);
+    } catch (error) {
+      console.error("Error saving data to AsyncStorage:", error);
+    }
+  };
+  
+  return (
+    <PeopleContext.Provider
+      value={{ people, pushPerson, updatePerson, removePerson, loadAsyncData }}
+    >
+      {children}
+    </PeopleContext.Provider>
   );
 }
